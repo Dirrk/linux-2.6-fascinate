5ac2f04b6c758da0906a02cd92a77564bf3eb24b
Dirrk <derekrada@gmail.com>
2010-10-08 03:29:55 -0400

    Fix bfq files

---
+++ include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 0 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 Fri Oct 8 03:06:02 EDT 2010"
+#define LINUX_COMPILE_TIME "03:06:02"
+#define LINUX_COMPILE_BY "derek"
+#define LINUX_COMPILE_HOST "derek-linux"
+#define LINUX_COMPILE_DOMAIN ""
+#define LINUX_COMPILER "gcc version 4.4.4 (Buildroot 2010.08) "
